942e8d68b0e144a5e1842cd89dd8f7b870d11661
Leon Sandøy <leon.sandoy@conmodo.com>
2020-04-27 22:22:33 +0200

    Nav complete, portrait started.

--- apps/main/views/home.py
+++ apps/main/views/home.py
@@ -1,7 +1,13 @@
+import random
+
 from django.shortcuts import render
 from django.views import View
 
 
 class Home(View):
     def get(self, request):
-        return render(request, "main/home.html")
+        random_brand = random.choice([
+            "images/nice_lemon.png",
+            "images/lemon_stegosaurus.png"
+        ])
+        return render(request, "main/home.html", {"brand": random_brand})
