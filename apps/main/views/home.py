0dcd413559390917a9ea56263267325cc73e964b
Leon Sandøy <leon.sandoy@conmodo.com>
2020-07-05 18:01:17 +0200

    Add navbar items with icons.

--- apps/main/views/home.py
+++ apps/main/views/home.py
@@ -7,7 +7,7 @@ from django.views import View
 class Home(View):
     def get(self, request):
         random_brand = random.choice([
-            "images/nice_lemon.png",
-            "images/lemon_stegosaurus.png"
+            "images/navbar/nice_lemon.png",
+            "images/navbar/lemon_stegosaurus.png"
         ])
         return render(request, "main/home.html", {"brand": random_brand})
